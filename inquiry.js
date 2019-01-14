e13418e96ac4cc63c77c6fe43376241e79aaecd8
守屋明穂 <moriyaakiho@moriyaakihonoMacBook-puro.local>
2019-01-14 12:27:25 +0900

    modified inqueryPage

---
+++ inquiry.js
@@ -0,0 +1,3 @@
+function order() {
+  alert ((document.getElementById("name").value) + "様、" + "色は" + (document.getElementById("color").value) + "、大きさは" + (document.getElementById("size").value) + "で承りました。" + "ご注文ありがとうございます。")
+}
