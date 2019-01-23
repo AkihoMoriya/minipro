73b104ddffb413b9fb82e3ab11a6e1190a8bb459
守屋明穂 <moriyaakiho@moriyaakihonoMacBook-puro.local>
2019-01-23 11:34:47 +0900

    js

--- inquiry.js
+++ inquiry.js
@@ -1,3 +1,9 @@
-function order() {
-  alert ((document.getElementById("name").value) + "様、" + "色は" + (document.getElementById("color").value) + "、大きさは" + (document.getElementById("size").value) + "で承りました。" + "ご注文ありがとうございます。")
+window.onload = function() {
+  var address, body;
+  var sendmail = document.getElementById("send");
+  sendmail.onclick  = function() {
+    body = (document.getElementById("name").value) + (document.getElementById("phone").value) + (document.getElementById("color").value) + (document.getElementById("size").value);
+    address = "t18809am@keio.jp"
+    location.href = "mailto" + address + "?body=" + body;
+  }
 }
--- top.js
+++ top.js
@@ -1,1 +1,9 @@
+function timeout_callback() {
+  if (window.confirm("作品紹介のページもぜひご覧ください！")){
+    location.href = "file:///Users/moriyaakiho/Desktop/minipro/listall.html";
+  } else {
+    window.alert("お時間ある時にぜひ！");
+  }
+}
 
+setTimeout(timeout_callback,20000);
